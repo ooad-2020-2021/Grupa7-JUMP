c75e64f6da88a89cc4fbb5653c4a651549ba2eb4
pozde <73055155+pozde@users.noreply.github.com>
2021-05-15 10:10:25 +0200

    Add files via upload

---
+++ CovidCentar/CovidCentar/Models/Korisnik.cs
@@ -0,0 +1,38 @@
+﻿using CovidCentar.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace CovidCentar.Models
+{
+    public class Korisnik:Osoba
+    {
+        [Required]
+        public bool punoljetan { get; }
+        [Required]
+        public String brojKnjizice { get; }
+        [Required]
+        public Uputnica uputnica { get; }
+        [Required]
+        public Karton karton { get; }
+
+        public Korisnik(int iD, string ime, string prezime, List<string> brojTelefon, string email, char spol, string adresa,bool punoljetan, string brojKnjizice, Uputnica uputnica, Karton karton):base(iD, ime, prezime, brojTelefon, email, spol, adresa)
+        {
+            this.punoljetan = punoljetan;
+            this.brojKnjizice = brojKnjizice;
+            this.uputnica = uputnica;
+            this.karton = karton;
+        }
+
+        public Simptomatika odaberiSimptome(Korisnik korisnik, DateTime datum1, bool odobren, List<SimptomEnum> uobicajeniSimptomi, string dodatniOpis, bool kontaktSaZarazenim, DateTime datum)
+        {
+            return new Simptomatika(korisnik, datum1, odobren, uobicajeniSimptomi, dodatniOpis, kontaktSaZarazenim, datum);
+        }
+
+        public ZahtjevZaVakcinu zahtjevZaVakcinaciju(DateTime datum,bool odobren,DateTime termin)
+        {
+            return new ZahtjevZaVakcinu(this, datum, odobren, termin);
+        }
+    }
+}
---
+++ CovidCentar/CovidCentar/Models/Osoba.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace CovidCentar.Models
+{
+    public class Osoba
+    {
+        
+        [Required]
+        [Key]
+        public int ID { get; }
+        [Required]
+        public String ime { get; }
+        [Required]
+        public String prezime { get; }
+        [Required]
+        public List<String> brojTelefon { get; }
+        [Required]
+        public String email { get; }
+        [Required]
+        public char spol { get; }
+        [Required]
+        public String adresa { get; }
+
+        public Osoba(int iD, string ime, string prezime, List<string> brojTelefon, string email, char spol, string adresa)
+        {
+            ID = iD;
+            this.ime = ime;
+            this.prezime = prezime;
+            this.brojTelefon = brojTelefon;
+            this.email = email;
+            this.spol = spol;
+            this.adresa = adresa;
+        }
+
+        public void dodajBrojTelefona(String broj)
+        {
+            brojTelefon.Add(broj);
+        }
+        public void izbrisiBrojTelefona(String broj)
+        {
+            if (brojTelefon.Contains(broj)){
+                brojTelefon.Remove(broj);
+            }
+            
+        }
+
+
+    }
+
+
+
+
+}
